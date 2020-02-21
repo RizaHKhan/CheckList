b6aca03ec8798e6f88502aba08e8c1f0f80446b7
Riza Khan <khanriza@gmail.com>
2020-02-21 00:26:53 -0500

    Added Relationship Between Models

--- server/models/Task.js
+++ server/models/Task.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const TaskSchema = new mongoose.Schema({
   description: {
     type: String,
     required: true
-  }
+  },
+  categories: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Task'
+  }]
 },
 {
   timestamps: true
